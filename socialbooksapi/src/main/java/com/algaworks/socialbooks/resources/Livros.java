b4067a7d12111bc8d072f65835c127ef799616e7
Bendazzoli <pauloh.bendazzoli@gmail.com>
2018-02-01 14:50:29 -0200

    Uso da @PathVariable, criando metodo "buscar"

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/Livros.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/Livros.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,5 +27,10 @@ public class Livros {
 	@RequestMapping(method = RequestMethod.POST)
 	public void salvar(@RequestBody Livro livro){
 		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{idLivro}", method = RequestMethod.GET)
+	public Livro buscar(@PathVariable("idLivro") Long idLivro){
+		return livrosRepository.findOne(idLivro);
 	}
 }
