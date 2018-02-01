441371790c8164a2172ee53a6994c27c77076cc0
Bendazzoli <pauloh.bendazzoli@gmail.com>
2018-02-01 16:31:57 -0200

    Uso de ResponseEntity HTTP 404 e 201

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/Livros.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/Livros.java
@@ -1,13 +1,17 @@
 package com.algaworks.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
@@ -25,13 +29,23 @@ public class Livros {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro){
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro){
 		livrosRepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/idLivro}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{idLivro}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("idLivro") Long idLivro){
-		return livrosRepository.findOne(idLivro);
+	public ResponseEntity<?> buscar(@PathVariable("idLivro") Long idLivro){
+		Livro livro = livrosRepository.findOne(idLivro);
+		
+		if(livro == null){
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value = "/{idLivro}", method = RequestMethod.DELETE)
