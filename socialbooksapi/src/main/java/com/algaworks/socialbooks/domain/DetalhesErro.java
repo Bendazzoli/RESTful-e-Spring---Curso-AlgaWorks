69d5719a79267a19332218af2efc23688fc469e0
Bendazzoli <pauloh.bendazzoli@gmail.com>
2018-02-02 10:40:51 -0200

    Criacao de classes para manipulação de ExceptionHandler e ControllerAdvice

---
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/DetalhesErro.java
@@ -0,0 +1,36 @@
+package com.algaworks.socialbooks.domain;
+
+
+public class DetalhesErro {
+	
+	private String titulo;
+	private Long status;
+	private Long timeStamp;
+	private String msgDesenvolvedor;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public Long getStatus() {
+		return status;
+	}
+	public void setStatus(long status) {
+		this.status = status;
+	}
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(Long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public String getMsgDesenvolvedor() {
+		return msgDesenvolvedor;
+	}
+	public void setMsgDesenvolvedor(String msgDesenvolvedor) {
+		this.msgDesenvolvedor = msgDesenvolvedor;
+	}
+}
