c5a1d72003c82ab0335799a6e76ba2c670798832
Bendazzoli <pauloh.bendazzoli@gmail.com>
2018-02-05 09:42:45 -0200

    Criacao da classe Autor

---
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Autor.java
@@ -0,0 +1,64 @@
+package com.algaworks.socialbooks.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Autor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idAutor;
+	
+	
+	private String nome;
+	
+	
+	private Date dtNascimento;
+	
+	
+	private String nacionalidade;
+	
+	@OneToMany(mappedBy = "autor")
+	@JsonIgnore
+	private List<Livro> livros;
+	
+	public Long getIdAutor() {
+		return idAutor;
+	}
+	public void setIdAutor(Long idAutor) {
+		this.idAutor = idAutor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(Date dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	public List<Livro> getLivros() {
+		return livros;
+	}
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+}
