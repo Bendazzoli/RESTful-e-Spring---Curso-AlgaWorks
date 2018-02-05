2a38e59fb5be082d33f6836f41bef0fccf549b3b
Bendazzoli <pauloh.bendazzoli@gmail.com>
2018-02-05 15:00:57 -0200

    Ajuste nos atributos - formantando as representações JSON

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Autor.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Autor.java
@@ -9,7 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @Entity
 public class Autor {
@@ -18,18 +21,20 @@ public class Autor {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idAutor;
 	
-	
+	@JsonInclude(Include.NON_NULL)
 	private String nome;
 	
-	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	@JsonInclude(Include.NON_NULL)
 	private Date dtNascimento;
 	
-	
+	@JsonInclude(Include.NON_NULL)
 	private String nacionalidade;
-	
+		
 	@OneToMany(mappedBy = "autor")
 	@JsonIgnore
 	private List<Livro> livros;
+	
 	
 	public Long getIdAutor() {
 		return idAutor;
