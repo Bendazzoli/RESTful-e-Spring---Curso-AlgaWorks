b3b5560962cf0a6d416eb683ff804fd2b3e60a58
Bendazzoli <pauloh.bendazzoli@gmail.com>
2018-02-08 09:42:47 -0200

    Criacao do Projeto socialbooksapi-client

---
+++ socialbooksapi-client/src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
@@ -0,0 +1,26 @@
+package com.algaworks.socialbooks.aplicacao;
+
+import java.net.URI;
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		
+		RestTemplate rest = new RestTemplate();
+		
+		RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/livros"))
+				                                   .header("Authorization", "Basic YWxnYXdvcmtzOnMzbmg0")
+				                                   .build();
+		
+		ResponseEntity<Livro[]> response = rest.exchange(request, Livro[].class);
+		
+		for(Livro livro : response.getBody()){
+			System.out.println("Livro: " + livro.getNome());
+			System.out.println("Editora: " + livro.getEditora());
+		}
+	}
+}
